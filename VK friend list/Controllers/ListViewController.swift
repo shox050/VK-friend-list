a51b2215055ce5301ee0f1ea37b470d1880cc2d4
shox050 <shox050@gmail.com>
2019-06-20 07:01:37 +0300

    did add ListVIewController class

---
+++ VK friend list/Controllers/ListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ListViewController.swift
+//  VK friend list
+//
+//  Created by Vladimir on 20/06/2019.
+//  Copyright © 2019 VladimirYakutin. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
