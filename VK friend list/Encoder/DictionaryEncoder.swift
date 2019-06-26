cc8d770e9d86a72a3a210bfd96d098246f8d8304
shox050 <shox050@gmail.com>
2019-06-26 23:12:18 +0300

    added DictionaryEncoder for encode struct to parameters for url

---
+++ VK friend list/Encoder/DictionaryEncoder.swift
@@ -0,0 +1,22 @@
+//
+//  FriendsConverter.swift
+//  VK friend list
+//
+//  Created by Vladimir on 26/06/2019.
+//  Copyright © 2019 VladimirYakutin. All rights reserved.
+//
+
+import Foundation
+
+class DictionaryEncoder {
+    func encode<T>(entity: T) -> [String:Any]? where T: Encodable {
+        let encoder = JSONEncoder()
+        
+        guard let data = try? encoder.encode(entity) else {
+            print("Encode failure")
+            return nil
+        }
+        
+        return try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String:Any]
+    }
+}
